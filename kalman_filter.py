ec9233e5de58018bbf9538563458085f823c837f
Edin Omerovic <edin.omerovic@enocean.com>
2023-08-03 12:28:13 +0200

    Updated the Kalman filter. It's probably not correctly implemented. Covariance matrix is not correct.

--- kalman_filter.py
+++ kalman_filter.py
@@ -17,6 +17,9 @@ class Kalman(object):
         # Second state variable: velocity
         self.current_estimate_x2 = initial_x1
         self.previous_estimate_x2 = initial_x1
+        # Third state variable: acceleration
+        self.current_estimate_x3 = initial_x1
+        self.previous_estimate_x3 = initial_x1
 
         #Variances
         # First state variance: position variance
@@ -26,33 +29,44 @@ class Kalman(object):
         self.current_variance_x2 = initial_p1
         self.previous_variance_x2 = initial_p1
 
+        self.current_variance_x3 = initial_p1
+        self.previous_variance_x3 = initial_p1
+
 
     '''
     Used to update the estimate based on the new measured data
     '''
-    def update(self, measured_value):
+    def update(self, measured_value, time_step):
         self.Kn1 = self.previous_variance_x1/(self.previous_variance_x1 + self.measurement_noise) 
         self.Kn2 = self.previous_variance_x2/(self.previous_variance_x2 + self.measurement_noise) 
+        self.Kn3 = self.previous_variance_x3/(self.previous_variance_x3 + self.measurement_noise) 
         
         # State update
-        self.current_estimate_x1 = self.previous_estimate_x1 + self.Kn2*(measured_value - self.previous_estimate_x1)
-        #self.current_estimate_x2 = self.previous_estimate_x2 + self.Kn*(measured_value - self.previous_estimate_x2)
+        self.current_estimate_x1 = self.previous_estimate_x1 + self.Kn3*(measured_value - self.previous_estimate_x1)
+        self.current_estimate_x2 = self.previous_estimate_x2 + self.Kn2*(measured_value - self.previous_estimate_x2)
         
         # Variance update
         self.current_variance_x1 = (1 - self.Kn1)*self.previous_variance_x1
         self.current_variance_x2 = (1 - self.Kn2)*self.previous_variance_x2
+        self.current_variance_x3 = (1 - self.Kn3)*self.previous_variance_x3
         
-        return self.current_estimate_x1
+        #Make it return all Kalman filter parameters, not just current estimate. 
+        return self
 
     '''
     Used to make predictions on future future states based on the dynamic model of the system
     '''
     def predict(self, time_step):
 
-        # State extrapolation
-        self.previous_estimate_x2 = self.current_estimate_x2
-        self.previous_estimate_x1 = self.current_estimate_x1 + time_step*self.current_estimate_x2
-
         # Variance extrapolation
-        self.previous_variance_x2 = self.current_variance_x2 + self.process_noise
-        self.previous_variance_x1 = self.current_variance_x1 + self.current_variance_x2*time_step**2
+        self.previous_variance_x3 = self.current_variance_x3 + self.process_noise
+        self.previous_variance_x2 = self.current_variance_x2 + self.previous_variance_x3*time_step**2 
+        self.previous_variance_x1 = self.current_variance_x1 + self.current_variance_x2*time_step**2 + self.previous_variance_x2*(0.5*time_step)**4
+
+        # State extrapolation
+        self.previous_estimate_x3 = self.current_estimate_x3
+        self.previous_estimate_x2 = self.current_estimate_x2 + time_step*self.previous_estimate_x3 
+        self.previous_estimate_x1 = self.current_estimate_x1 + time_step*self.previous_estimate_x2 + 0.5*self.previous_estimate_x3*time_step**2
+
+        
+        return self
